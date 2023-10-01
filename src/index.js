34eaa6f2e0a6771eaea0fc537705fdf51c037102
Felix Avalos <felavalosg@gmail.com>
2023-10-01 15:24:08 -0600

    exercises 1.2 finished

--- src/index.js
+++ src/index.js
@@ -9,12 +9,18 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <Part part={props.part} exercises={props.exercises}/>
     </div>
   )
 }
@@ -22,7 +28,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.excersices1 + props.excersices2 + props.excersices3}</p>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
     </div>
   )
 }
@@ -42,7 +48,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
-      <Total excersices1={exercises1} excersices2={exercises2} excersices3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
