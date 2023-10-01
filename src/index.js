d4058abcf4a297eea429dede6cbdd7e3bb393944
Felix Avalos <felavalosg@gmail.com>
2023-10-01 15:39:29 -0600

    exercise 1.2. Optimized

--- src/index.js
+++ src/index.js
@@ -30,25 +30,21 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application devlopment'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of component'
-  const exercises3 = 14
+  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of component']
+  const exercises = [10, 7, 14]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
     </div>
   )
 }
